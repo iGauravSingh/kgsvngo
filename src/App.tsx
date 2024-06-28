72efbc8d37a052a927beadc03538276678ce54fe
iGauravSingh <poetonroad@gmail.com>
2024-06-28 14:11:23 +0530

    removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,6 @@
 import "./App.css";
 import "./custom.css";
-import Achivers from "./components/Achivers";
-import Activity from "./components/Activity";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Support from "./components/Support";
-import Tagline from "./components/Tagline";
-import Zig from "./components/Zig";
-import Hero from "./components/section/Hero";
 
-import { VideoCarousel } from "./components/section/Video-carousel";
 import { Outlet } from "react-router-dom";
 
 
