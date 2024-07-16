67f7fc4bcc3e7ff9736aef3148e2d1163d71d883
iGauravSingh <poetonroad@gmail.com>
2024-07-16 15:14:40 +0530

    changed remaining text sizes in about

--- src/pages/aboutus/WhoAreWe.tsx
+++ src/pages/aboutus/WhoAreWe.tsx
@@ -24,7 +24,7 @@ const WhoAreWe = () => {
           <p className="text-gray-700 leading-7 tracking-wide">
           Khushhal Gram Samaj Vikas Foundation was incorporated as a 'not for profit organization' on February 5th, 2019 under the Companies Act, 2013, to assist, support and promote rural development, more specifically  primary and junior school education, clean & green environment, nutrition and health care, agriculture, women empowerment, skilful employment etc in villages. It is an endeavour to create modern, model villages. The Corporate Identity number of the Company is U74994UP2019NPL113235.  
           </p>
-          <p className="text-gray-700 text-lg mt-4 leading-7 tracking-wide">We are a group of friends from different fields and with varied interests that have come together for a common cause – rural development & prosperity. We believe that prosperity and happiness in India can be achieved only if needs of its agrarian society is fulfilled. Prosperity in villages would ease the migratory pressure in cities and they could be better managed.</p>
+          <p className="text-gray-700 mt-4 leading-7 tracking-wide">We are a group of friends from different fields and with varied interests that have come together for a common cause – rural development & prosperity. We believe that prosperity and happiness in India can be achieved only if needs of its agrarian society is fulfilled. Prosperity in villages would ease the migratory pressure in cities and they could be better managed.</p>
         </div>
       </div>
     </div>
